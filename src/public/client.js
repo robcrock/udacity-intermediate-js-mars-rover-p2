dedb8b42eeec0b5a0082bc9294b9b81d37a843d1
robcrockPI <robert@propellerindustries.com>
2020-08-21 07:38:25 -0400

    Modify solution approach

--- src/public/client.js
+++ src/public/client.js
@@ -16,35 +16,43 @@ const updateStore = (store, newState) => {
   render(root, store);
 };
 
+const curiosityClickHandler = () => {
+  console.log('clicked');
+  // render(root, store);
+}
+
 const render = async (root, state) => {
   root.innerHTML = App(state);
+
+  const curiosityButton = document.querySelector('#curiosity');
+  if (curiosityButton) {
+    curiosityButton.addEventListener('click', curiosityClickHandler);
+  }
+//   window.setTimeout(()=>{
+// },50);
 };
 
 // create content
 const App = state => {
   const { rovers, apod } = state;
 
+  // Add to template literal for potential testing
+  // {ImageOfTheDay(apod)}
+
+
+
   return `
-        <header></header>
+        <header>
+          <button id='curiosity'>Curiosity</button>
+        </header>
         <main>
             ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                <!-- {ImageOfTheDay(apod)} -->
-                ${RoverData(rovers[0])}
-            </section>
+            ${RoverData(rovers[0])}
         </main>
         <footer></footer>
     `;
+
+
 };
 
 // listening for load event because page should load before any JS is called
@@ -53,6 +61,8 @@ window.addEventListener('load', () => {
   // getRoverData(store.rovers[0]);
   render(root, store);
 });
+
+
 
 // ------------------------------------------------------  COMPONENTS
 // TODO
@@ -98,31 +108,8 @@ const ImageOfTheDay = apod => {
 };
 
 const RoverData = rover => {
-  // If image does not already exist, or it is not from today -- request it again
-  // const today = new Date();
-  // const photodate = new Date(apod.date);
-  // console.log(photodate.getDate(), today.getDate());
-
-  // console.log(photodate.getDate() === today.getDate());
-  // if (!apod || apod.date === today.getDate()) {
-  //   getImageOfTheDay(store);
-  // }
-
   const roverPhoto = getRoverData(rover);
-  console.log(roverPhoto.r);
-
-  // check if the photo of the day is actually type video!
-  // if (apod.media_type === 'video') {
-  //   return `
-  //           <!-- <p>See today's featured video <a href="${apod.url}">here</a></p> -->
-  //           <!-- <p>${apod.title}</p> -->
-  //           <!-- <p>${apod.explanation}</p> -->
-  //       `;
-  // }
-  // return `
-  //           <!-- <img src="${apod.image.url}" height="350px" width="100%" /> -->
-  //           <!-- <p>${apod.image.explanation}</p> -->
-  //       `;
+  console.log(roverPhoto);
 };
 
 // ------------------------------------------------------  API CALLS
