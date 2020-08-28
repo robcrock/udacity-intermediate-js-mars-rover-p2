8193203c53a0a1078bab800d1437f2130bae2486
robcrockPI <robert@propellerindustries.com>
2020-08-28 07:35:07 -0400

    Updating code for submission.

--- src/public/client.js
+++ src/public/client.js
@@ -8,7 +8,8 @@ const store = {
   rovers: ['Curiosity', 'Opportunity', 'Spirit'],
   tab: '', // w
   pageName: '', //
-  roverData: {},
+  roverData: null,
+  roverPhotos: [],
 };
 
 // add our markup to the page
@@ -20,134 +21,28 @@ const updateStore = (store, newState) => {
   render(root, store);
 };
 
-// const curiosityClickHandler = () => {
-//   console.log('clicked');
-//   // render(root, store);
-// }
-
 // W3Schools tab reference:
 // https://www.w3schools.com/howto/howto_js_full_page_tabs.asp
-
-function openPage(pageName, elmnt, color) {
+function openPage(pageName) {
   updateStore(store, { pageName });
-
-  // // Hide all elements with class="tabcontent" by default */
-  // let i;
-
-  // const tabcontent = document.getElementsByClassName('tabcontent');
-  // for (i = 0; i < tabcontent.length; i++) {
-  //   tabcontent[i].style.display = 'none';
-  // }
-
-  // // Remove the background color of all tablinks/buttons
-  // const tablinks = document.getElementsByClassName('tablink');
-  // for (i = 0; i < tablinks.length; i++) {
-  //   tablinks[i].style.backgroundColor = '';
-  // }
-
-  // // Show the specific tab content
-  // document.getElementById(pageName).style.display = 'block';
-  // document.getElementById(pageName).style.backgroundColor = color;
-
-  // // Add the specific color to the button used to open the tab content
-  // // console.log(elmnt);
-  // elmnt.style.backgroundColor = color;
-
-  // console.log(RoverData('curiosity'));
 }
 
 const render = async (root, state) => {
   root.innerHTML = App(state);
-
-  // Get the element with id="defaultOpen" and click on it
-  // document.getElementById('defaultOpen').click();
-};
-
-const getRoverData = rover => {
-  // const rover = state;
-  fetch(`http://localhost:3000/rover`)
-    .then(response => response.json())
-    .then(r => {
-      let roversByName = {
-        // curiosity: {}
-      };
-      r.rovers.forEach(rover => {
-        roversByName[rover.name.toLowerCase()] = rover;
-      });
-      console.log('roversByName:', roversByName);
-      const {max_date} = roversByName[rover];
-      fetch(`http://localhost:3000/rover/${rover}/${max_date}`)
-        .then(response => response.json())
-        .then(roverPhotos => {
-          // console.log('photos ', roverPhotos);
-          updateStore(store, { roverData: roversByName[rover] });
-        });
-      // console.log(r);
-    });
-
-  // fetch(`http://localhost:3000/rover/${rover}`)
-  //   .then(res => res.json())
-  //   .then(r => {
-  //     console.log(r);
-  //     // Think about adding a fetch here
-  //     updateStore(store, { r });
-  //   });
-};
-
-let called = null;
-const RoverData = rover => {
-  // console.log('ROVERDATA', rover, store.r);
-  if (called !== rover) {
-    // console.log('called', called, rover);
-    called = rover;
-    getRoverData(rover);
-  }
-  if (!store.r) {
-    return `<h1>Loading...</h1>`;
-  }
-  return `
-    <div class="tabcontent">
-      <h1>${rover}</h1>
-      <img src="${store.r.photos[0].img_src}" height="350px" width="100%" />
-      <ul>
-        <li>Launch date ${store.r.photos[0].rover.launch_date}</li>
-        <li>Landing date  ${store.r.photos[0].rover.landing_date}</li>
-        <li>Status ${store.r.photos[0].rover.status}</li>
-      </ul>
-    </div>
-    `;
 };
 
 // create content
 const App = state => {
   const { rovers, apod, pageName } = state;
-  console.log(pageName);
+  const activeRoverArr = rovers.filter(name => pageName === name.toLowerCase());
   return `
-    <button class="tablink" onclick="openPage('pod', this, 'red')" id="defaultOpen">Picture of the Day</button>
-    <button class="tablink" onclick="openPage('curiosity', this, 'green')">Curiosity</button>
-    <button class="tablink" onclick="openPage('spirit', this, 'orange')">Spirit</button>
-    <button class="tablink" onclick="openPage('opportunity', this, 'blue')">Opportunity</button>
+    <button class="tablink" onclick="ImageOfTheDay('apod')">Picture of the Day</button>
+    <button class="tablink" onclick="openPage('curiosity')">Curiosity</button>
+    <button class="tablink" onclick="openPage('spirit')">Spirit</button>
+    <button class="tablink" onclick="openPage('opportunity')">Opportunity</button>
 
-    ${pageName === 'pod' ? ImageOfTheDay(apod) : ''}
-
-      ${
-        pageName === rovers[0].toLowerCase()
-          ? RoverData(rovers[0].toLowerCase())
-          : ''
-      }
-
-      ${
-        pageName === rovers[1].toLowerCase()
-          ? RoverData(rovers[1].toLowerCase())
-          : ''
-      }
-
-      ${
-        pageName === rovers[2].toLowerCase()
-          ? RoverData(rovers[2].toLowerCase())
-          : ''
-      }
-    `;
+    ${activeRoverArr[0] ? RoverData(activeRoverArr[0].toLowerCase()) : ''}
+  `;
 };
 
 // listening for load event because page should load before any JS is called
@@ -156,22 +51,19 @@ window.addEventListener('load', () => {
 });
 
 // ------------------------------------------------------  COMPONENTS
-// TODO
-// Create a tabbed nav for each rover in the store
-
 // Example of a pure function that renders infomation requested from the backend
 const ImageOfTheDay = apod => {
   // If image does not already exist, or it is not from today -- request it again
   const today = new Date();
   const photodate = new Date(apod.date);
-  // console.log(photodate.getDate(), today.getDate());
-
-  // console.log(photodate.getDate() === today.getDate());
   if (!apod || photodate === today.getDate()) {
     getImageOfTheDay(store);
+    console.log('Apod: , apod');
   }
 
-  console.log(apod.image.url);
+  if (!apod) {
+    return `<h1>Loading...</h1>`;
+  }
   // check if the photo of the day is actually type video!
   if (apod.media_type === 'video') {
     return `
@@ -182,17 +74,38 @@ const ImageOfTheDay = apod => {
       </div>
         `;
   }
+  // return `
+      // <div id="pod" class="tabcontent">
+      //     <img src="${apod.image.url}" height="350px" width="100%" />
+      //     <p>${apod.image.explanation}</p>
+      // </div>            
+      //   `;
+};
+
+let called = null;
+const RoverData = rover => {
+  if (called !== rover) {
+    called = rover;
+    getRoverData(rover);
+  }
+  if (!store.roverData || !store.roverPhotos.length) {
+    return `<h1>Loading...</h1>`;
+  }
   return `
-      <div id="pod" class="tabcontent">
-          <img src="${apod.image.url}" height="350px" width="100%" />
-          <p>${apod.image.explanation}</p>
-      </div>            
-        `;
+    <div class="tabcontent">
+      <h1>${store.roverData.name}</h1>
+      <ul>
+        <li>Launch date ${store.roverData.launch_date}</li>
+        <li>Landing date  ${store.roverData.landing_date}</li>
+        <li>Status ${store.roverData.status}</li>
+        <li>Most recent photos taken on ${store.roverData.max_date}</li>
+      </ul>
+      ${RoverImages(store.roverPhotos)}
+    </div>
+    `;
 };
 
 // ------------------------------------------------------  API CALLS
-
-// Example API call
 const getImageOfTheDay = state => {
   const { apod } = state;
 
@@ -202,3 +115,35 @@ const getImageOfTheDay = state => {
       updateStore(store, { apod });
     });
 };
+
+const getRoverData = rover => {
+  fetch(`http://localhost:3000/rover`)
+    .then(response => response.json())
+    .then(r => {
+      const roversByName = {
+        // curiosity: {}
+      };
+
+      r.rovers.forEach(rover => {
+        roversByName[rover.name.toLowerCase()] = rover;
+      });
+      const { max_date } = roversByName[rover];
+      fetch(`http://localhost:3000/rover/${rover}/${max_date}`)
+        .then(response => response.json())
+        .then(roverPhotos => {
+          updateStore(store, {
+            roverData: roversByName[rover],
+            roverPhotos: roverPhotos.photos.map(photo => photo.img_src),
+          });
+        });
+    });
+};
+
+// ------------------------------------------------------  OTHER FUNCTIONS
+function RoverImages(imgArray) {
+  let output = '';
+  imgArray.forEach(img => {
+    output += `<img src="${img}" height="350px" width="100%" />`;
+  });
+  return output;
+}
